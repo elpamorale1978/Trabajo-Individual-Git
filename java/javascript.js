189b228ce2db9c8a8b35caf23561c0cd9eb58096
Pablo <pamorale1978@gmail.com>
2021-09-11 17:20:58 -0300

    Agrega Boton
    Agrega Boton de cambio fondo de pantalla

--- java/javascript.js
+++ java/javascript.js
@@ -1,1 +1,16 @@
-alert("Hola. Bienvenido a mi sitio")
+alert("Hola. Bienvenido a mi sitio");
+
+
+
+function generarNuevoColor(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 16)];
+	}
+
+	document.body.style.background = color;
+	document.getElementById("text").innerHTML = "Copiar Color";
+}
